a92a57878afaabd55fb7fbfb2c9aea584cdf12ec
DK\VIAF <viaf@sitecore.net>
2018-01-19 12:18:54 +0200

    #Code withot fix

---
+++ src/Sitecore.Support.202620/LatestEventsReportProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Cintel.Reporting.DTOs;
+using Sitecore.Cintel.Reporting.Services;
+using Sitecore.Cintel.Reporting.Services.Mappers;
+using Sitecore.Diagnostics;
+using Sitecore.Cintel.Reporting;
+
+namespace Sitecore.Support.Cintel.Reporting.Processors
+{
+  public class LatestEventsReportProcessor
+  {
+    public LatestEventsReportProcessor()
+        : this(ViewFactory.GetLatestEventsReportService(), ViewFactory.GetEventToDataTableMapper())
+    {
+    }
+
+    public LatestEventsReportProcessor(IEventsReportService service, IMapper<List<EventDTO>, DataTable> mapper)
+    {
+      Assert.ArgumentNotNull(service, nameof(service));
+      Assert.ArgumentNotNull(mapper, nameof(mapper));
+      Service = service;
+      Mapper = mapper;
+    }
+    private IEventsReportService Service { get; }
+
+    private IMapper<List<EventDTO>, DataTable> Mapper { get; }
+
+    public void Process(ReportProcessorArgs args)
+    {
+      Assert.ArgumentNotNull(args, nameof(args));
+      var contactId = args.ReportParameters.ContactId;
+      Guid interactionId;
+
+      List<EventDTO> data;
+      if (!string.IsNullOrEmpty(args.ReportParameters.ViewEntityId) &&
+          Guid.TryParse(args.ReportParameters.ViewEntityId, out interactionId))
+        data = Service.GetReport(new EventsReportServiceParameters(contactId, interactionId));
+      else
+        data = Service.GetReport(new EventsReportServiceParameters(contactId));
+
+      args.ResultTableForView = Mapper.Map(data);
+    }
+  }
+}
---
+++ src/Sitecore.Support.202620/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.202620")]
+[assembly: AssemblyProduct("Sitecore.Support.202620")]
+[assembly: ComVisible(false)]
